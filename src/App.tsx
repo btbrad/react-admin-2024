b320c90a8e458b9f4e94501b1ad3b2c90d730269
btbrad <24836323+btbrad@users.noreply.github.com>
2024-03-05 21:37:56 +0800

    引入antd并解决样式冲突

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from 'antd'
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline-light-300 text-purple-600">
         Hello world!
       </h1>
+      <Button type="primary">Button</Button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import 'normalize.css'
 import './index.css'
 import App from './App.tsx'
+import { StyleProvider } from '@ant-design/cssinjs'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StyleProvider hashPriority="high">
+      <App />
+    </StyleProvider>
   </React.StrictMode>
 )
