b4a64818c64d89c2f9a161eaf0c2542b4019f2a0
btbrad <24836323+btbrad@users.noreply.github.com>
2024-03-22 22:04:25 +0800

    add json-server

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 1000 * 5,
   headers: {
     'Content-Type': 'application/json;charset=utf-8'
