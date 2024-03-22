96158c188573b010742b52a0508a8541dac7098c
btbrad <24836323+btbrad@users.noreply.github.com>
2024-03-22 22:41:05 +0800

    axios interceptor

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, { InternalAxiosRequestConfig, AxiosResponse } from 'axios'
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -9,39 +9,41 @@ const instance = axios.create({
 })
 
 instance.interceptors.request.use(
-  config => {
-    console.log(config)
-    config.headers['Authorization'] = 'Bearer ' + localStorage.getItem('token')
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token
+    }
     return config
   },
   error => {
     console.log(error)
+    return Promise.reject(error)
   }
 )
 
 instance.interceptors.response.use(
-  response => {
-    console.log('res', response)
-    const { data } = response
-    if (data.code !== 200) {
-      console.log('报错')
-      if (data.code === 401) {
-        console.log('token过期')
-      }
+  (response: AxiosResponse) => {
+    const { code, msg } = response.data
+    if (code === 200) {
+      return response.data
     }
-    return Promise.resolve(data)
+    console.log(msg || '系统出错')
+    return Promise.reject(new Error(msg || 'Error'))
   },
   error => {
-    let { message } = error
-    if (message == 'Network Error') {
-      message = '后端接口连接异常'
-    } else if (message.includes('timeout')) {
-      message = '系统接口请求超时'
-    } else if (message.includes('Request failed with status code')) {
-      message = '系统接口' + message.substr(message.length - 3) + '异常'
+    if (error.response.status === 401) {
+      // todo 登录过期
+      // ElMessageBox.confirm('登录状态过期，请重新登录', '提示', {
+      //   confirmButtonText: '确定',
+      //   showCancelButton: false,
+      //   type: 'warning'
+      // }).then(() => {
+      //   removeAll()
+      //   router.push('/login')
+      // })
     }
-    console.log('报错')
-    return Promise.reject(error)
+    return Promise.reject(error.message)
   }
 )
 
