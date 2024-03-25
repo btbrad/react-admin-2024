864058725f97e515c372f2bc79f2303edc194b13
btbrad <24836323+btbrad@users.noreply.github.com>
2024-03-25 22:58:14 +0800

    接口类型定义

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,16 @@
 import { request } from '@/utils'
+import type { AxiosPromise } from 'axios'
 
-console.log(request)
-export const login = (data: any) => {
+interface LoginData {
+  username: string
+  password: string
+}
+
+interface LoginResult {
+  token: string
+}
+
+export const login = (data: LoginData): AxiosPromise<LoginResult> => {
   return request({
     method: 'post',
     url: '/auth/login',
